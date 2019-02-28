0ba50f93827f2becfdc582cf8d295e25290bf451
CoCoManYY <1344483030@qq.com>
2019-02-28 10:46:09 +0800

    fix some bugs

--- bin/creat-react-myy.js
+++ bin/creat-react-myy.js
@@ -1,19 +1,21 @@
+#!/usr/bin/env node
 process.env.NODE_PATH=__dirname+'/../node_modules';
 const  {resolve} = require('path');
 const res = command => resolve(__dirname,'../commands',command);
 const program = require('commander');
 
-program.version(require('../pakage').version);
+program.version(require('../package').version )
 
-program.usage('<command>');
+program.usage('<command>')
 
 program.command('init')
-    .option('-f,--foo','enable some foo')
+    .option('-f, --foo', 'enable some foo')
     .description('Generate a new project')
     .alias('i')
     .action(() => {
         require(res('init'))
-    });
-if(!program.args.length){
+    })
+
+if(!program._args.length){
     program.help()
 }
